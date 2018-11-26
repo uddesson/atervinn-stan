25d25b45fa9a0245f8cde5b8976b953685c2d8ff
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-11-26 11:43:43 +0100

    Add message component #4 #3

--- app/components/UI/index.js
+++ app/components/UI/index.js
@@ -2,3 +2,4 @@ export { SearchInput } from './SearchInput';
 export { colors } from './colors';
 export { utilityStyles } from './utilityStyles';
 export { Header, SubHeading, Paragraph, ParagraphBold } from './Types';
+export { Message } from './Message';
