2560abca7d7a501f0bca8d5dadea5218e9cef9b0
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-11-27 18:03:52 +0100

    Basic styling for modal #6

--- app/components/UI/SearchResultModal.js
+++ app/components/UI/SearchResultModal.js
@@ -14,13 +14,16 @@ import { SubHeading, Paragraph, ParagraphBold } from './Types';
 export const SearchResultModal = props => {
   const { isVisible, handleModal } = props;
   return (
-    <View style={styles.wrapper}>
-      <Modal visible={isVisible} transparent={true}>
+    <View>
+      <Modal
+        visible={isVisible}
+        transparent={true}
+        backdropColor="black"
+        style={styles.modal}
+      >
         <View style={styles.wrapper}>
-          <SubHeading style={styles.subheading}>
-            Namn på det klickade listobjektet
-          </SubHeading>
-          <Paragraph>Detta ska sorteras som</Paragraph>
+          <SubHeading style={styles.subheading}>Plastbestick</SubHeading>
+          <Paragraph>Detta ska sorteras som plast.</Paragraph>
           <Image style={{ width: 50, height: 50 }} source={{ uri: 'plast' }} />
           <TouchableOpacity onPress={() => handleModal(false)}>
             <Text>Stäng</Text>
@@ -33,13 +36,20 @@ export const SearchResultModal = props => {
 
 const styles = StyleSheet.create({
   wrapper: {
-    margin: 10,
-  },
-  modal: {
-    margin: 100,
-    color: 'red',
+    marginTop: 50,
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#ddd',
+    shadowColor: '#ddd',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
   },
   subheading: {
-    paddingTop: 20,
+    paddingTop: 10,
   },
 });
