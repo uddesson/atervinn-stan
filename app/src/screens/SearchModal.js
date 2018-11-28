c22c3fec5ec8db8802fa5b944051b36b749af962
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-11-28 15:51:51 +0100

    Less styling for modal #6

--- app/src/screens/SearchModal.js
+++ app/src/screens/SearchModal.js
@@ -1,61 +1,62 @@
 import React from 'react';
 import { View, TouchableOpacity, StyleSheet, Image, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { utilityStyles, colors, Paragraph, SubHeading } from '../components/UI';
 
 export const SearchModal = props => {
   const { navigation } = props;
   const itemTitle = navigation.getParam('itemTitle');
+  const sortingCategory = navigation.getParam('sortingCategory');
 
   return (
-    <View style={styles.wrapper}>
-      <View style={[utilityStyles.row, utilityStyles.justifyBetween]}>
-        <SubHeading style={styles.subheading}>Plastbestick</SubHeading>
-        <Image style={styles.image} source={{ uri: 'plast' }} />
+    <SafeAreaView style={styles.wrapper}>
+      <View style={[utilityStyles.justifyBetween, utilityStyles.flex]}>
+        <View style={[utilityStyles.row, utilityStyles.justifyBetween]}>
+          <SubHeading style={[styles.subheading, utilityStyles.capitalizeText]}>
+            {itemTitle}
+          </SubHeading>
+          {/* TODO: add image for 'övrigt avfall' */}
+          <Image style={styles.image} source={{ uri: sortingCategory }} />
+        </View>
+        <Paragraph style={styles.paragraph}>
+          {itemTitle} kan återvinnas i stan. Sorteras som {sortingCategory}
+        </Paragraph>
       </View>
-      <Paragraph style={styles.paragraph}>
-        Plastbestick kan återvinnas i stan. Sorteras som plast.
-        {itemTitle}
-      </Paragraph>
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.7}
-        onPress={() => navigation.navigate('Home')}
-      >
-        <Text style={utilityStyles.whiteText}>Stäng</Text>
-      </TouchableOpacity>
-    </View>
+
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Text style={utilityStyles.whiteText}>Stäng</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
-    marginTop: 50,
+    marginTop: 20,
     marginRight: 10,
     marginLeft: 10,
     padding: 10,
-    backgroundColor: colors.white,
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: colors.lightGrey,
-    shadowColor: colors.lightGrey,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 2,
   },
   subheading: {
-    color: colors.greenDark,
+    color: colors.darkGreen,
   },
   image: {
-    width: 40,
-    height: 40,
+    width: 60,
+    height: 60,
   },
   paragraph: {
     marginTop: 5,
   },
   button: {
     borderWidth: 1,
-    borderColor: colors.green,
-    backgroundColor: colors.green,
+    borderColor: colors.darkGreen,
+    backgroundColor: colors.darkGreen,
     borderRadius: 5,
     padding: 5,
     marginTop: 15,
