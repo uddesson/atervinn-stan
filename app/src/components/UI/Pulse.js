ef6bb7ecf195a1ce8baac87f012e7497812528a0
Emma Uddesson <emma.uddesson@gmail.com>
2018-11-29 15:41:19 +0100

    Add drafts for pulse animation #18
    Credits to https://github.com/mastermoo/react-native-pulse-loader

---
+++ app/src/components/UI/Pulse.js
@@ -0,0 +1,92 @@
+// @flow
+import React, { Component } from 'react';
+import { View, TouchableOpacity, Animated } from 'react-native';
+import { PulseAnimation } from './PulseAnimation';
+import { colors } from './colors';
+
+/**
+ * Credit for pulse and animation goes to https://github.com/mastermoo/react-native-pulse-loader.
+ * Component have been modified to fit the purposes of this app.
+ */
+
+type Props = {
+  interval: number,
+  size: number,
+  pulseMaxSize: number,
+  icon: Component,
+  borderColor: string,
+  backgroundColor: string,
+  getStyle: Function,
+};
+
+export class Pulse extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      circles: [],
+    };
+
+    this.counter = 1;
+    this.setInterval = null;
+    this.anim = new Animated.Value(1);
+  }
+
+  componentDidMount() {
+    this.setCircleInterval();
+  }
+
+  setCircleInterval() {
+    const { interval } = this.props;
+    this.setInterval = setInterval(this.addCircle.bind(this), interval);
+    this.addCircle();
+  }
+
+  addCircle() {
+    const { circles } = this.state;
+    this.setState({ circles: [...circles, this.counter] });
+    this.counter++;
+  }
+
+  render() {
+    const { icon } = this.props;
+
+    return (
+      <View
+        style={{
+          backgroundColor: 'transparent',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderWidth: 1,
+        }}
+      >
+        {this.state.circles.map(circle => (
+          <PulseAnimation key={circle} {...this.props} />
+        ))}
+
+        <TouchableOpacity
+          activeOpacity={1}
+          style={{
+            transform: [
+              {
+                scale: this.anim,
+              },
+            ],
+          }}
+        >
+          {icon}
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+Pulse.defaultProps = {
+  interval: 2000,
+  size: 50,
+  pulseMaxSize: 150,
+  icon: undefined,
+  borderColor: colors.darkBlue,
+  backgroundColor: colors.blue,
+  getStyle: undefined,
+};
--- app/src/components/UI/index.js
+++ app/src/components/UI/index.js
@@ -1,15 +1,13 @@
 export { SearchInput } from './SearchInput';
 export { colors, gradients } from './colors';
 export { utilityStyles } from './utilityStyles';
-export { Header, SubHeading, Paragraph, ParagraphBold } from './Types';
+export {
+  Header, SubHeading, Paragraph, ParagraphBold,
+} from './Types';
 export { Message } from './Message';
 export {
-  HeartIcon,
-  SadIcon,
-  SearchIcon,
-  MapIcon,
-  InfoIcon,
-  GpsIcon,
+  HeartIcon, SadIcon, SearchIcon, MapIcon, InfoIcon, GpsIcon,
 } from './Icons';
 export { SearchResultList } from './SearchResultList';
 export { ShortcutBanner } from './ShortcutBanner';
+export { Pulse } from './Pulse';
