7de64920110d08ee356b5f77587ea26c4a4aed7e
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-11-29 15:08:43 +0100

    Add symbol explanation in info-screen #14

--- app/src/screens/Info.js
+++ app/src/screens/Info.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { Paragraph, utilityStyles } from '../components/UI';
+import {
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+import {
+  Paragraph,
+  Heading,
+  SubHeading,
+  utilityStyles,
+  colors,
+} from '../components/UI';
+import { symbolExplanation } from '../components/UI';
 
 export class Info extends Component {
   render() {
     return (
-      <SafeAreaView style={[styles.screen, utilityStyles.center]}>
-        <Paragraph>I'm the info screen</Paragraph>
+      <SafeAreaView style={[styles.screen]}>
+        <View style={[styles.container, utilityStyles.center]}>
+          <Heading>Symbolförklaring</Heading>
+          <FlatList
+            style={styles.listContainer}
+            data={symbolExplanation}
+            keyExtractor={item => item.title}
+            renderItem={({ item }) => {
+              return (
+                <View style={[utilityStyles.row]}>
+                  <Image style={styles.image} source={{ uri: item.icon }} />
+                  <SubHeading
+                    style={[
+                      utilityStyles.capitalizeText,
+                      utilityStyles.alignSelfEnd,
+                      styles.subHeading,
+                    ]}
+                  >
+                    {item.title}
+                  </SubHeading>
+                  <Paragraph style={styles.text}>{item.text}</Paragraph>
+                </View>
+              );
+            }}
+          />
+
+          <Paragraph>Material hämtat från FTI, HSR, SVOA</Paragraph>
+        </View>
       </SafeAreaView>
     );
   }
@@ -15,5 +55,24 @@ export class Info extends Component {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    padding: 10,
+    backgroundColor: colors.whiteSmoke,
+  },
+  container: {
+    padding: 10,
+  },
+  listContainer: {
+    backgroundColor: colors.white,
+    borderRadius: 5,
+    padding: 10,
+    height: 400,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    marginRight: 10,
+  },
+  text: {
+    margin: 10,
   },
 });
