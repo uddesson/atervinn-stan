b4cf2d3cc06e18aff86c18601fcdbe9f42d9a55b
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-05 20:54:49 +0100

    Further styling of modal #47

--- app/src/components/UI/MapModal.js
+++ app/src/components/UI/MapModal.js
@@ -1,8 +1,10 @@
+//@flow
 import React, { Component } from 'react';
 import { View, Modal, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { colors } from './colors';
 import { utilityStyles } from './utilityStyles';
 import { SubHeading, Paragraph } from './Types';
+import { CancelIcon, SuccessIcon, WarningIcon } from './Icons';
 
 type Props = {
   visible: boolean,
@@ -18,8 +20,9 @@ type Props = {
 
 export const MapModal = (props: Props) => {
   const { visible, onPress, marker } = props;
+  // how should glass be rendered?
   const sortingOptions = marker.sorting.map(option => {
-    return <Image style={styles.image} source={{ uri: option }} />;
+    return <Image key={option} style={styles.image} source={{ uri: option }} />;
   });
 
   return (
@@ -27,27 +30,62 @@ export const MapModal = (props: Props) => {
       <Modal visible={visible} transparent={true} animationType={'none'}>
         <View style={[utilityStyles.flex1, utilityStyles.center]}>
           <View style={styles.modalContent}>
-            <SubHeading style={utilityStyles.capitalizeText}>
-              {marker.address}
-            </SubHeading>
-            <Paragraph>
-              {marker.sortingConfirmed
-                ? 'Bekräftad position'
-                : 'Ej bekräftad position'}
-            </Paragraph>
-
-            {marker.sortingConfirmed ? (
-              <View>
-                <Paragraph>Möjliga sorteringsalternativ</Paragraph>
-                <View style={utilityStyles.row}>{sortingOptions}</View>
-              </View>
-            ) : (
-              <Paragraph>Sorteringsalternativ är ej bekräftad</Paragraph>
-            )}
-
-            <TouchableOpacity>
-              <Paragraph onPress={onPress}>Stäng</Paragraph>
+            {/* should user also be able to press background to close? */}
+            <TouchableOpacity onPress={onPress} style={styles.button}>
+              <CancelIcon width={20} height={20} fill={colors.darkGreen} />
             </TouchableOpacity>
+
+            <View
+              style={[
+                utilityStyles.justifyBetween,
+                utilityStyles.col,
+                utilityStyles.flex1,
+              ]}
+            >
+              <View>
+                <SubHeading style={utilityStyles.capitalizeText}>
+                  {marker.address || marker.stationName}
+                </SubHeading>
+                {/* fti-positions don't have this flag so we want don't to check this
+              if we don't we will get a negative false  */}
+                {marker.hasOwnProperty('locationConfirmed') ? (
+                  marker.locationConfirmed ? (
+                    // is this implicit?
+                    <View style={utilityStyles.row}>
+                      <Paragraph>Bekräftad position</Paragraph>
+                      <SuccessIcon width={20} height={20} fill={colors.green} />
+                    </View>
+                  ) : (
+                    <View style={utilityStyles.row}>
+                      <Paragraph>Ej bekräftad position</Paragraph>
+                      <WarningIcon width={20} height={20} fill={colors.red} />
+                    </View>
+                  )
+                ) : null}
+
+                {sortingOptions.length > 0 ? (
+                  <View>
+                    <Paragraph>
+                      Möjliga sorteringsalternativ på stationen
+                    </Paragraph>
+                    <View style={[utilityStyles.row, styles.imageWrapper]}>
+                      {sortingOptions}
+                    </View>
+                  </View>
+                ) : (
+                  <Paragraph>Sorteringsalternativ är ej bekräftad</Paragraph>
+                )}
+              </View>
+
+              {marker.hasOwnProperty('locationConfirmed') ? (
+                !marker.locationConfirmed || !marker.sortingConfirmed ? (
+                  <Paragraph style={styles.finePrint}>
+                    På grund av otillräcklig information kan inte modulens
+                    plats/sorteringsalternativ garanteras.
+                  </Paragraph>
+                ) : null
+              ) : null}
+            </View>
           </View>
         </View>
       </Modal>
@@ -63,15 +101,33 @@ const styles = StyleSheet.create({
     height: 50,
   },
   modalContent: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.whiteSmoke,
-    height: 300,
+    backgroundColor: colors.white,
+    shadowColor: colors.lightGrey,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 2,
+    shadowOpacity: 0.8,
+    minHeight: 300,
+    maxHeight: 500,
     width: '90%',
     borderRadius: 5,
+    padding: 15,
+  },
+  button: {
+    marginLeft: 'auto',
+  },
+  imageWrapper: {
+    marginTop: 10,
   },
   image: {
-    width: 40,
-    height: 40,
+    width: 70,
+    height: 70,
+    marginRight: 5,
+    marginTop: 5,
+  },
+  finePrint: {
+    fontSize: 12,
   },
 });
