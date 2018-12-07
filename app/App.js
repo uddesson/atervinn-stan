02307a3898dcfc9cfc0519f929c4395b339a1386
Emma Uddesson <emma.uddesson@gmail.com>
2018-12-07 21:15:07 +0100

    Add setup to view network requests in dev tools

--- app/App.js
+++ app/App.js
@@ -10,6 +10,16 @@ import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { AppNavigator } from './src/components/AppNavigator';
 
+/**
+ * NOTES:
+ * GLOBAL.XMLHttpRequest lets us view network requests from the app in Chrome Dev Tools.
+ * The FlowFix has been added as the third instance of GLOBAL caused an error
+ * that we haven't found a good solution for yet.
+ */
+
+// $FlowFixMe
+GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest; // Enable XMLHttpRequest to show in debugger.
+
 const RootNavigator = createAppContainer(AppNavigator);
 
 export default class App extends Component<{}> {
