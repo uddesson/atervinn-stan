86a59044aae4bd8bcfc886de96127857d61b72ce
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-10 11:26:04 +0100

    Add util to calculate stationtype

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -2,3 +2,4 @@ export { calcColor } from './calcColor';
 export { toUpperCase } from './toUpperCase';
 export { initialRegion } from './getRegion';
 export { getIconCode } from './getIconCode';
+export { getStationSymbol } from './getStationSymbol';
