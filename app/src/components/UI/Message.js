64a779b6fea27e98f8bf739cc283f4ef75dcdc47
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-11 15:20:44 +0100

    Refactor to keep DRY #64

--- app/src/components/UI/Message.js
+++ app/src/components/UI/Message.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Paragraph } from './Types';
+import { HeartIcon } from './Icons';
+import { colors } from './colors';
+import { utilityStyles } from './utilityStyles';
 
 type Props = {
   type: string,
@@ -9,20 +12,34 @@ type Props = {
 
 export const Message = (props: Props) => {
   const { type } = props;
-  const examples = 'Ett sökord kan t.ex. vara...';
-  const error = 'Vi hittar tyvärr inte det du söker.';
-  const message = type === 'examples' ? examples : error;
 
-  const externalUrl = 'https://tinyurl.com/y99gjgz4';
-  return (
-    <View>
-      <Paragraph>{message}</Paragraph>
-    </View>
-  );
+  const renderMessage = () => {
+    if (type === 'no results') {
+      return (
+        <>
+          <Paragraph style={styles.message}>
+            Vi hittar tyvärr inte det du söker i vår databas. Det betyder dock
+            inte att det inte går att återvinna!
+          </Paragraph>
+          <HeartIcon width={50} height={50} fill={colors.red} />
+        </>
+      );
+    } else if (type === 'examples') {
+      /* 
+        TODO: render som examples.
+      */
+      return <Paragraph>Exempel på sökord</Paragraph>;
+    }
+  };
+
+  return <View style={utilityStyles.center}>{renderMessage()}</View>;
 };
 
 const styles = StyleSheet.create({
   wrapper: {
     justifyContent: 'center',
+  },
+  message: {
+    margin: 8,
   },
 });
