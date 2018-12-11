e1b23101dec4064e048a662565a9f77bfb6e5eb2
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-11 15:24:13 +0100

    Rename variables

--- app/src/utils/getSearchModalMessage.js
+++ app/src/utils/getSearchModalMessage.js
@@ -1,25 +1,28 @@
+//@flow
 import React from 'react';
 import { moduleSorting, ftiStationSorting } from './sortingTypes';
 
-export const getSearchModalMessage = (sortingType: string) => {
+export const getSearchModalMessage = (sortingType: string, title: string) => {
+  /* 
+    if sortingType matches moduleSorting and/or ftiStationSorting
+    it can be recycled at the stations we map out.
+  */
   const moduleSortingAvailable = moduleSorting.includes(sortingType);
   const ftiStationSortingAvailable = ftiStationSorting.includes(sortingType);
 
-  const messageIfBothAvailable =
-    'kan återvinnas i stan, både på en modul och på en fti station och sorteras som';
-  const messageIfModule = 'kan återvinnas i stan vid en modul och sorteras som';
-  const messageIfFti =
-    'kan återvinnas i stan vid en fti station och sorteras som';
-  const messageIfUnavailable =
-    'måste återvinnas på återvinningscentral och sorteras där som';
+  /* messages to user inside searchModal.js */
+  const availableMessage = `${title} kan återvinnas i stan, både på en modul och på en FTI Station och sorteras som ${sortingType}.`;
+  const unavailableMessage = `${title} måste återvinnas på återvinningscentral.`;
+  const moduleMessage = `${title} kan återvinnas i stan vid en modul och sorteras som ${sortingType}.`;
+  const ftiMessage = `${title} kan återvinnas i stan vid en FTI Station och sorteras som ${sortingType}.`;
 
   if (moduleSortingAvailable && ftiStationSortingAvailable) {
-    return messageIfBothAvailable;
+    return availableMessage;
   } else if (moduleSortingAvailable) {
-    return messageIfModule;
+    return moduleMessage;
   } else if (ftiStationSortingAvailable) {
-    return messageIfFti;
+    return ftiMessage;
   } else {
-    return messageIfUnavailable;
+    return unavailableMessage;
   }
 };
