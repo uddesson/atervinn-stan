e8365cf82bb4cba87a1d727203d43762f4378dc2
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-12 14:59:52 +0100

    Add function that throws errors #61

--- server.js
+++ server.js
@@ -16,8 +16,15 @@ app.listen(port, () => console.log(`Server listening on port ${port}`));
  * TODO: Handle errors for all endpoints.
  */
 
+// util function to throw errors to keep code DRY
+const throwError = (code, message) => {
+  const err = new Error(message);
+  err.status = code;
+  throw err;
+};
+
 // Search in sortingdata.
-app.get("/api/sorting/search/:query", (req, res) => {
+app.get('/api/sorting/search/:query', (req, res) => {
   let query = req.params.query;
   let results = [];
 
