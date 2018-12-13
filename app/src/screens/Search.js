445d68e74e85860df3f28b31ba8aaa7492824804
Emma Uddesson <emma.uddesson@gmail.com>
2018-12-13 14:30:45 +0100

    Handle errors and inavlid search input #72

--- app/src/screens/Search.js
+++ app/src/screens/Search.js
@@ -1,6 +1,6 @@
 //@flow
 import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView, View } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Alert, ActivityIndicator } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import {
   utilityStyles,
@@ -8,7 +8,8 @@ import {
   SearchResultList,
   colors,
   CloseButton,
-  Message,
+  SearchResultMessage,
+  Paragraph,
 } from '../components/UI';
 
 type Props = {
@@ -16,6 +17,8 @@ type Props = {
 };
 
 type State = {
+  error: boolean,
+  loading: boolean,
   searchInput: string,
   searchResults: Object[],
 };
@@ -24,30 +27,50 @@ export class Search extends Component<Props, State> {
   static navigationOptions = { header: null };
 
   state = {
+    error: false,
+    loading: false,
     searchInput: '',
     searchResults: [],
   };
 
   handleSearchInput = (searchInput: string) => {
+    // Always set the searchInput to state, even if it's empty. Controls the output and message to user.
     this.setState({ searchInput });
+
+    // But if the searchInput is empty, don't try to fetch data. Will only result in 404 and catch error, unnecessary.
+    if (searchInput.trim() === '') return;
     this.getSearchResuts(searchInput);
   };
 
   getSearchResuts = async (searchInput: string) => {
+    this.setState({ loading: true });
     try {
       const res = await fetch(`http://localhost:5000/api/sorting/search/${searchInput}`);
       const searchResults = await res.json();
-      // Store searchresults in state
-      this.setState({ searchResults });
-    } catch (error) {
-      // TODO: Handle errors
-      console.log(error);
+      this.setState({ searchResults, loading: false });
+    } catch (err) {
+      this.setState({ loading: false, error: true });
     }
+  };
+
+  handleSearchOutPut = () => {
+    const { searchInput, searchResults, error, loading } = this.state;
+
+    if (error) return <SearchResultMessage type={'error'} />;
+    if (!error && loading) return <ActivityIndicator size={'large'} style={{ marginTop: '45%' }} />;
+    if (searchInput.length > 1 && searchResults.length === 0)
+      return <SearchResultMessage type={'no-results'} />;
+    if (!searchInput)
+      return (
+        <Paragraph>{`(Tillfällig text utan styling.) Visa exempeldata här ifall servern är igång`}</Paragraph>
+      );
+
+    return <SearchResultList results={searchResults} />;
   };
 
   render() {
     const { navigation } = this.props;
-    const { searchInput, searchResults } = this.state;
+    const { searchInput, searchResults, error, loading } = this.state;
 
     return (
       <SafeAreaView style={[utilityStyles.flex1, utilityStyles.fullWidth, styles.screen]}>
@@ -63,13 +86,7 @@ export class Search extends Component<Props, State> {
             <SearchInput navigation={navigation} onChangeText={this.handleSearchInput} />
             <CloseButton onPress={() => navigation.goBack()} />
           </View>
-          {searchInput.length === 0 ? (
-            <Message type={'examples'} />
-          ) : searchResults.length === 0 ? (
-            <Message type={'no results'} />
-          ) : (
-            <SearchResultList results={searchResults} navigation={navigation} />
-          )}
+          {this.handleSearchOutPut()}
         </View>
       </SafeAreaView>
     );
@@ -81,6 +98,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.whiteSmoke,
   },
   container: {
+    marginTop: 20,
     paddingLeft: 20,
     paddingRight: 20,
   },
