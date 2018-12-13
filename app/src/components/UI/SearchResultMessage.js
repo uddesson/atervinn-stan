206c81fe44e2881a256f0262ad567a829b0ec588
Emma Uddesson <emma.uddesson@gmail.com>
2018-12-13 14:31:51 +0100

    Rename Message to SearchResultMessage + update content
    Removed icon, didn't look good with the message.

---
+++ app/src/components/UI/SearchResultMessage.js
@@ -0,0 +1,35 @@
+//@flow
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Paragraph } from './Types';
+import { HeartIcon } from './Icons';
+import { colors } from './colors';
+import { utilityStyles } from './utilityStyles';
+
+type Props = {
+  type: string,
+};
+
+export const SearchResultMessage = (props: Props) => {
+  const { type } = props;
+  const message =
+    type === 'no-results'
+      ? `Vi hittar tyvärr inte det du söker i vår databas. Det betyder dock inte att det inte går att återvinna!`
+      : `Ops! Något har gått snett – kan inte hämta resultat just nu.`;
+
+  return (
+    <View style={styles.container}>
+      <Paragraph style={[styles.message, utilityStyles.lineHeightNormal]}>{message}</Paragraph>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: '10%',
+  },
+  message: {
+    margin: 8,
+    textAlign: 'center',
+  },
+});
