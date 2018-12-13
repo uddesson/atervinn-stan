21f04168d18ee46a96085f29a6e89b3e4a472b36
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-13 14:12:54 +0100

    Refactor to less lines of code #63

--- app/src/utils/checkCurrentSeason.js
+++ app/src/utils/checkCurrentSeason.js
@@ -5,37 +5,19 @@ import twix from 'twix';
 
 /*
  * Function looks at current date and match the the date ranges representing seasons
- * and returns a string whitch is used as a image url
+ * and returns a string which is used as a image url.
  */
 export const checkCurrentSeason = () => {
-  // Uncomment and replace to test
-  //const today = moment('2018 12 02');
+  const spring = moment('03 01', 'MM DD').twix('05 31', 'MM DD');
+  const summer = moment('06 01', 'MM DD').twix('08 31', 'MM DD');
+  const fall = moment('09 01', 'MM DD').twix('11 30', 'MM DD');
+  const winter = moment('12 01', 'MM DD').twix('02 28', 'MM DD');
 
-  const spring = moment('03 01', 'MM DD')
-    .twix('05 31', 'MM DD')
-    .isCurrent();
-  //.contains(today);
-
-  const summer = moment('06 01', 'MM DD')
-    .twix('08 31', 'MM DD')
-    .isCurrent();
-  //.contains(today);
-
-  const fall = moment('09 01', 'MM DD')
-    .twix('11 30', 'MM DD')
-    .isCurrent();
-  //.contains(today);
-
-  const winter = moment('12 01', 'MM DD')
-    .twix('02 28', 'MM DD')
-    .isCurrent();
-  //.contains(today);
-
-  if (spring) {
+  if (spring.isCurrent()) {
     return 'spring';
-  } else if (summer) {
+  } else if (summer.isCurrent()) {
     return 'summer';
-  } else if (fall) {
+  } else if (fall.isCurrent()) {
     return 'fall';
   } else {
     return 'winter';
