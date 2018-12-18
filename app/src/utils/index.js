0e4237712903178bfd3f1d0b2e14220cd82f83da
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2018-12-18 22:04:22 +0100

    Update exports

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,8 +1,8 @@
-export { checkColor } from './checkColor';
+export { getSortingColor } from './getSortingColor';
 export { toUpperCase } from './toUpperCase';
 export { initialRegion } from './getRegion';
 export { getStationSymbol } from './getStationSymbol';
 export { getSearchModalMessage } from './getSearchModalMessage';
 export { parseArray } from './parseArray';
 export { checkModuleAvailability } from './checkModuleAvailability';
-export { checkCurrentSeason } from './checkCurrentSeason';
+export { getCurrentSeason } from './getCurrentSeason';
